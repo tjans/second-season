6a286d446dd8999d87cf551f7342e21d30d3e47a
tkj <traviskjansen@gmail.com>
2026-01-03 18:48:36 -0600

    Refactor: update react-hook-form to version 7.69.0, enhance form handling in TeamEdit component, and implement new UI components for improved structure and validation

--- src/pages/Teams/TeamEdit.tsx
+++ src/pages/Teams/TeamEdit.tsx
@@ -1,32 +1,48 @@
 import usePageTitle from '@/hooks/usePageTitle'
 import ContentWrapper from "@/components/ContentWrapper";
+
+// Form handling and validation
 import { useForm, SubmitHandler } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from '@/components/ui/Button';
 import * as z from "zod";
+
+// UI components
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/input";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
 
 // Zod schema for form validation
 const team = z.object({
-    city: z.string().min(1, "ZCity is required"),
+    city: z.string().min(1, "City is required"),
+    abbreviation: z.string().min(1, "Abbreviation is required").max(4, "Abbreviation must be at most 4 characters"),
+    mascot: z.string().optional(),
 });
 
 // Infer the form data type from the Zod schema
 type teamType = z.infer<typeof team>;
 
-// Begin component
+
+// Begin component -----------------------------------------
 export default function TeamEdit() {
     usePageTitle("");
 
     // Set the resolver
     const form = useForm<teamType>({
         resolver: zodResolver(team),
+        defaultValues: {
+            city: "",
+            abbreviation: "",
+            mascot: "",
+        } as teamType,
     });
-
-    // Register the form
-    const {
-        register,
-        formState: { errors },
-    } = form;
 
     const onSubmit: SubmitHandler<teamType> = (data) => {
         console.log(data)
@@ -35,13 +51,46 @@ export default function TeamEdit() {
     return (
         <>
             <ContentWrapper>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="max-w-md mx-auto mt-8">
-                    <input {...register("city")} className="border border-gray-300 rounded px-2 py-1" />
-                    {/* errors will return when field validation fails  */}
-                    <div>{errors.city && <span>{errors.city.message}</span>}</div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
 
-                    <div className="my-4"><Button type="submit">Submit</Button></div>
-                </form>
+                        <FormField
+                            control={form.control}
+                            name="city"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>City</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}>
+                        </FormField>
+
+                        <FormField
+                            control={form.control}
+                            name="abbreviation"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Abbreviation</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} maxLength={4} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        3-4 letter team abbreviation.
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}>
+                        </FormField>
+
+                        <Button type="submit">Submit</Button>
+
+                    </form>
+                </Form>
             </ContentWrapper>
         </>
     );
